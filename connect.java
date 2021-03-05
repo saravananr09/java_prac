5072327b0641441f115005a4581a6c1417453d3f
vanansa <saravananr6332@gmail.com>
2021-03-05 12:08:03 +0530

    connecting mysql and java

--- connect.java
+++ connect.java
@@ -4,21 +4,23 @@ import java.util.concurrent.TimeUnit;
 class connect {
     public static void main(String[] args) {
       try {
-          String url = "jdbc:mysql://localhost:3378/test?autoReconect=true&useSSL=false";
+          String url = "jdbc:mysql://localhost:3378/test?socket=/tmp/mysql78.sock?autoReconect=true&useSSL=false";
           String uname = "saravanan";
           String password = "password";
         //   String socket = "/tmp/mysql78.sock";
-          String query = "select categoryName from test.ces limit 1";
+          // String query = "select categoryName from test.ces order by categoryId desc limit ";
+          String query = "select @@socket";
         // Connection con = DriverManager.getConnection("Jdbc:mysql://localhost:3378/world", "saravanan","password" );
         Class.forName("com.mysql.jdbc.Driver");
 
         Connection con = DriverManager.getConnection(url,uname,password);
         System.out.println("Connection got established !!!!");
         Statement st = con.createStatement();
+        System.out.println(query);
         ResultSet rs = st.executeQuery(query);
 
          rs.next(); 
-        String name = rs.getString("CategoryName");
+        String name = rs.getString("@@socket");
         TimeUnit.SECONDS.sleep(2);
         System.out.println(name);
         st.close();
